8b06ccd2daa101feb466031cf6d1d09da509b4df
Yaksien <71828472+Yaksien@users.noreply.github.com>
2020-11-12 11:12:11 +0100

    winkewagen lichaam
    lichaam gemaakt voor winkelwagen

--- cart.php
+++ cart.php
@@ -1,7 +1,45 @@
 <?php
 include __DIR__ . "/header.php";
 ?>
+<html>
+<body>
+<style>
 
+    div.fixed {
+        position: fixed;
+        bottom: 0;
+        padding-left: 0;
+        width: 200px;
+
+    }
+
+        </style>
+
+
+
+<!-- Title Section -->
+<header>
+    <div class="title1"><h2>Winkelwagen</h2></div>
+    <div class="title2"> <h4>Hieronder uw producten</h4></div>
+</header>
+
+<!-- Body section -->
+<div class = "body_sec">
+    <section id="Content">
+        <h3>Content section</h3>
+    </section>
+</div>
+
+<!-- button section -->
+<div class="fixed">
+    <button  type="button">Verder met winkelen</button>
+    <button  type="button">Naar betaling</button>
+</div>
+
+
+
+</body>
+</html>
 <?php
 include __DIR__ . "/footer.php";
 ?>
