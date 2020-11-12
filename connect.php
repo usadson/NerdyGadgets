a7f98b6314da0c46e75e00fd492557dad260a708
Tristan <code@thewoosh.me>
2020-11-12 09:12:53 +0100

    Initial Commit

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+global $Connection;
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Set MySQLi to throw exceptions
+try {
+    $Connection = mysqli_connect("localhost", "tager", "", "nerdygadgets");
+    mysqli_set_charset($Connection, 'latin1');
+    $DatabaseAvailable = true;
+} catch (mysqli_sql_exception $e) {
+  var_dump($e);    
+
+	$DatabaseAvailable = false;
+}
+if (!$DatabaseAvailable) {
+    ?><h2>Website wordt op dit moment onderhouden.</h2><?php
+    die();
+}
