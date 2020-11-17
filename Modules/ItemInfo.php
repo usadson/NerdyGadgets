62b116c4da49cfc8b4f4ad65d9b159a9c93ebf9a
Vincent <70948074+MysteryFade@users.noreply.github.com>
2020-11-17 10:21:51 +0100

    Image toegevoegd

--- Modules/ItemInfo.php
+++ Modules/ItemInfo.php
@@ -8,16 +8,20 @@
  */
 include_once "../connect.php";
 function ItemInfo($productID) {
-	$Query = "SELECT StockItemID, StockItemName, RecommendedRetailPrice FROM stockitems WHERE StockItemID = '" . $productID . "'";
+	$Query = "SELECT S.StockItemID, StockItemName, RecommendedRetailPrice, ImagePath
+	FROM stockitems S
+	JOIN stockitemimages I ON I.StockItemID = S.StockItemID
+	WHERE StockItemID = '" . $productID . "'
+	";
 	$Statement = mysqli_prepare($Connection, $Query);
 	mysqli_stmt_execute($Statement);
 	$HeaderItems = mysqli_stmt_get_result($Statement);
 
 /*foreach ($HeaderItems as $HeaderItem) {*/
-    $ID = $HeaderItem['StockItemID'];
+    $ID = $HeaderItem['S.StockItemID'];
     $Name = $HeaderItem['StockItemName'];
     $Price = $HeaderItem['RecommendedRetailPrice'];
+	$Image = $HeaderItem['ImagePath'];
     /*print($ID . "|" . $Name . "|" . $Price . "<br>"); -- TEST print*/
 /*}*/
-
 ?>
