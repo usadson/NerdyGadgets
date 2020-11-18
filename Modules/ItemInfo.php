a43e5e3ffd484ee764d13b9adc099185f7d96672
Tristan <code@thewoosh.me>
2020-11-18 11:00:48 +0100

    [ItemInfo] Improve documentation and type safety

--- Modules/ItemInfo.php
+++ Modules/ItemInfo.php
@@ -14,14 +14,27 @@ class ItemInformation {
   public $Price;
   public $Image;
 
-  public function __construct($name, $price, $image) {
+  public function __construct(string $name, float $price, string $image) {
     $this->Name = $name;
     $this->Price = $price;
     $this->Image = $image;
   }
 }
 
-function GetPrimaryImage($connection, $productID) {
+/**
+ * GetPrimaryImage.
+ *
+ * Het plaatje die gepakt wordt uit de stockitems.ImagePath.
+ *
+ * @param mysqli $connection De connectie die gebruikt wordt om met de
+ *                           nerdygadgets database te communiceren
+ * @param int $productID De identificatie nummer van een product dat overeenkomt
+ *                       met `stockitems.StockItemID`
+ *
+ * @return string|null het pad relatieve pad naar het plaatje van de item
+ *                     vanaf de nerdygadgets/Public/ map.
+ */
+function GetPrimaryImage(mysqli $connection, int $productID) {
   $query = "SELECT ImagePath
   FROM stockitemimages
 	WHERE StockItemID = " . $productID;
@@ -37,7 +50,21 @@ function GetPrimaryImage($connection, $productID) {
   return null;
 }
 
-function GetSecondaryImage($connection, $productID) {
+/**
+ * GetSecondaryImage.
+ *
+ * Het plaatje die gepakt wordt uit de stockgroups.ImagePath als het andere
+ * plaatje niet gevonden kon worden.
+ *
+ * @param mysqli $connection De connectie die gebruikt wordt om met de
+ *                           nerdygadgets database te communiceren
+ * @param int $productID De identificatie nummer van een product dat overeenkomt
+ *                       met `stockitems.StockItemID`
+ *
+ * @return string|null het pad relatieve pad naar het plaatje van de item
+ *                     vanaf de nerdygadgets/Public/ map.
+ */
+function GetSecondaryImage(mysqli $connection, int $productID) {
   $query = "SELECT ImagePath
             FROM stockgroups
             JOIN stockitemstockgroups ON StockItemID = " . $productID . "
@@ -64,10 +91,15 @@ function GetSecondaryImage($connection, $productID) {
  * maar ook meer dan 1, dus we pakken altijd de eerste stockgroup, net als in
  * cart.php
  *
+ * @param mysqli $connection De connectie die gebruikt wordt om met de
+ *                           nerdygadgets database te communiceren
+ * @param int $productID De identificatie nummer van een product dat overeenkomt
+ *                       met `stockitems.StockItemID`
+ *
  * @return string (non-null) het pad relatieve pad naar het plaatje van de item
  *                vanaf de nerdygadgets/Public/ map.
  */
-function GetItemImage($connection, $productID) {
+function GetItemImage(mysqli $connection, int $productID) {
   if (($primary = GetPrimaryImage($connection, $productID)) != null) {
     return $primary;
   }
@@ -80,21 +112,31 @@ function GetItemImage($connection, $productID) {
 }
 
 /**
+ * ItemInfo
  *
+ * Haal informatie op van een item met stockitems.StockItemID de $productID is.
+ *
+ * @param mysqli $connection De connectie die gebruikt wordt om met de
+ *                           nerdygadgets database te communiceren
+ * @param int $productID De identificatie nummer van een product dat overeenkomt
+ *                       met `stockitems.StockItemID`
+
+ *
+ * @return ItemInformation De class met informatie over het item
  */
-function ItemInfo($Connection, $productID) {
+function ItemInfo(mysqli $connection, int $productID) {
 	$Query = "SELECT StockItemName, RecommendedRetailPrice
 	FROM stockitems S
 	WHERE S.StockItemID = " . $productID;
 
-	$Statement = mysqli_prepare($Connection, $Query);
+	$Statement = mysqli_prepare($connection, $Query);
 	mysqli_stmt_execute($Statement);
   $HeaderItem = mysqli_stmt_get_result($Statement);
   $item = mysqli_fetch_assoc($HeaderItem);
 
   $Name = $item['StockItemName'];
   $Price = $item['RecommendedRetailPrice'];
-  $Image = GetItemImage($Connection, $productID);
+  $Image = GetItemImage($connection, $productID);
 
   return new ItemInformation($Name, $Price, $Image);
 }
