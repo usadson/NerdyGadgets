a6b7f26bfc58a3de6ac402a7e5d94c34a1e4c200
Tristan <code@thewoosh.me>
2020-11-19 19:11:49 +0100

    Add functionality to 'Remove' button that removes a product from the shoppping cart

--- cart.php
+++ cart.php
@@ -56,96 +56,8 @@ img{
                     </thead>
                     <tbody>
                     <?php
-                    function PrintProductRow($productID, $image, $name, $isInStock, $quantity, $price) {
-                      /*
-                       * <tr>
-                        <td><img src="https://dummyimage.com/50x50/55595c/fff" /> </td>
-                        <td>Placeholder</td>
-                        <td>In stock</td>
-                        <td></td>
-                        <td class="text-right">000 €</td>
-                    </tr>
-                       * */
-
-
-
-
-
-                      	$quantity = 1;
-			$qp = $price * $quantity;
-
-                      print("<tr>");
-
-                      print("<td><img src=\"Public/" . $image . "\" alt=\"Product Plaatje\"></td>");
-                      print("<td>" . $name . "</td>");
-                      print("<td>" . ($isInStock ? "Op voorraad" : "Niet op voorraad") . "</td>");
-                      print('<td><input class="form-control" type="text" value="' . $quantity . '" /></td>');
-                      print("<td class=\"text-right\">€ " . $qp . '</td>');
-                      print('<td class="text-right"><button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> </button></td>');
-
-                      print("</tr>");
-
-
-
-                    }
-
-
-                    function MapProductIdWithQuantity($products) {
-                      $map = array();
-
-                      foreach ($products as $product) {
-                        if (!array_key_exists($product, $map)) {
-                          $map[$product] = 1;
-                        } else {
-                          $map[$product]++;
-                        }
-                      }
-
-                      return $map;
-                    }
-
-
-                    include_once('Modules/ItemInfo.php');
-
-                    $isCartEmpty = !isset($_SESSION["cart"]) || empty($_SESSION["cart"]);
-
-                    if (!$isCartEmpty) {
-                      $map = MapProductIdWithQuantity($_SESSION["cart"]);
-
-                      foreach ($map as $productID => $quantity) {
-                        $itemInformation = ItemInfo($Connection, $productID);
-
-                        PrintProductRow($productID, $itemInformation->Image, $itemInformation->Name, true, $quantity, $itemInformation->Price);
-                      }
-                    }
-
-
+                    include("Modules/ComputeShoppingCartTable.php");
                     ?>
-
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>Sub-Total</td>
-                        <td class="text-right">000 €</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>BTW</td>
-                        <td class="text-right">000 €</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td><strong>Total</strong></td>
-                        <td class="text-right"><strong>000 €</strong></td>
-                    </tr>
                     </tbody>
                 </table>
             </div>
@@ -153,13 +65,7 @@ img{
     </div>
 </div>
 
-<?php
-
-if ($isCartEmpty) {
-  print("<i>Je hebt nog geen producten toegevoegd aan je winkelwagen.</i>");
-}
-
-?>
+<i id="noProductsWarning" style="opacity:0">Je hebt nog geen producten toegevoegd aan je winkelwagen.</i>
 
 <!-- button section -->
 <div style="text-align: center">
@@ -170,13 +76,127 @@ if ($isCartEmpty) {
         <button  type="button">Naar betaling </button>
     </a>
 	<form action="Interface/TruncateCart.php" method="post">
-        <input  type="submit" value="Verwijder alles"></button>
+        <input  type="submit" value="Verwijder alles">
     </form>
 
 
 </div>
 
+<?php
+/**
+ * PREFACE: Het verwijderen van een enkel product van de pagina.
+ * --------------------------------------------------------------------------
+ * Het verwijderen van een product moet gedaan worden met PHP, want dit is de
+ * rol die de session (dus de winkelwagen gegevens) beheert.
+ *
+ * Nu is het probleem dat PHP wordt uitgevoerd voordat de pagina naar de
+ * browser wordt gestuurd. Om dit te 'omzeilen' kunnen we gebruik maken van
+ * van <form>s. Dit kunnen we doen op twee manieren:
+ *
+ * 1. Bij elk product een nieuwe <form> te gebruiken en daar een
+ * <input type="hidden" value="..."> te plaatsen met als waarde het product ID.
+ * Dit is een heel lelijke manier om het te doen, en dit valt ook onder
+ * DOM-bloating.
+ *
+ * 2. Bij elk product een custom attribuut te gebruiken, met als waarde de
+ * product ID. (data-product-id="..."). Dan hebben we onderaan de pagina een
+ * JavaScript blok die voor elk element met zo'n attribuut een actie geeft voor
+ * als je op de 'Verwijder' knop drukt.
+ *
+ *
+ *
+ */
+?>
+<script>
+  /**
+   * Pak elk <td> element met een data-product-id attribuut.
+   */
+  let collection = document.querySelectorAll("td[data-product-id]");
 
+  /**
+   * De status als een item wordt verwijdert of niet.
+   *
+   * Als je een item verwijdert, duurt het even om te laden. Als de gebruiker
+   * in de tussentijd nog een product verwijdert, kan het niet helemaal
+   * kloppen.
+   */
+  let isInProgress = false;
+
+  function activateProgress() {
+    if (isInProgress) {
+        return false;
+    }
+
+    isInProgress = true;
+    console.log("Progress: activating...");
+
+    for (let i = 0; i < collection.length; i++) {
+      let trashButton = collection[i].querySelector("i.fa-trash");
+      trashButton.parentElement.style.backgroundColor = "#aaa";
+      trashButton.enabled = false;
+    }
+
+    console.log("Progress: activated");
+
+    return true;
+  }
+
+  function deactivateProgress() {
+    console.log("Progress: deactivating... ");
+    for (let i = 0; i < collection.length; i++) {
+        let trashButton = collection[i].querySelector("i.fa-trash");
+        trashButton.parentElement.style.backgroundColor = "";
+        trashButton.enabled = true;
+    }
+
+    console.log("Progress: deactivated");
+    isInProgress = false;
+  }
+
+  /**
+   * Deze functie zorgt ervoor dat de melding dat-er-geen-producten-in-de
+   * winkelwagen-zitten wordt weergegeven als er geen producten zijn, of anders
+   * verstopt hij deze weer.
+   */
+  function tryUpdateWarningMessage(productCount) {
+    let element = document.getElementById("noProductsWarning");
+
+    if (productCount.toString() === "0") {
+      element.style.opacity = "1";
+    } else {
+      element.style.opacity = "0";
+    }
+  }
+
+  /**
+   * Dit is de functie die aangeroepen wordt als er op de 'Verwijder' knop
+   * gedrukt wordt.
+   */
+  function deleteItem(element) {
+    if (!activateProgress()) {
+        return;
+    }
+
+    let productID = element.parentElement.getAttribute("data-product-id");
+    fetch("Interface/RemoveItem.php?productID=" + productID)
+      .then(result => {
+          tryUpdateWarningMessage(result.headers.get("X-NG-Products"));
+          return result.text();
+      })
+      .then(data => {
+        /**
+         * Interface/RemoveItem.php geeft ons de nieuwe tabel weer terug,
+         * en we vervangen de oude hiermee:
+         */
+        document.querySelector("tbody").innerHTML = data;
+
+        deactivateProgress();
+      });
+  }
+
+  tryUpdateWarningMessage(<?php global $productCount; echo $productCount; ?>);
+
+</script>
 
 </body>
 </html>
