ffd609dc6f3b757313bf4bf13d94f2b4895aaf99
Vincent <70948074+MysteryFade@users.noreply.github.com>
2020-11-24 14:06:39 +0100

    Meerdere checks toegevoegd

--- new_account.php
+++ new_account.php
@@ -25,24 +25,36 @@ FORM {
  
  
 <label for="fname" align="left">Wachtwoord</label><br>
- <input type="text" id="Wachtwoord1" name="Wachtwoord1" value="Wachtwoord"><br><br>
+ <input type="text" id="Wachtwoord1" name="Wachtwoord1"><br><br>
  
  <label for="fname" align="left">Herhaling wachtwoord</label><br>
- <input type="text" id="Wachtwoord2" name="Wachtwoord2" value="Herhaal wachtwoord"><br><br>
+ <input type="text" id="Wachtwoord2" name="Wachtwoord2"><br><br>
     <input type="submit">
  </form>
  <?php
 
- if($_GET['Wachtwoord2'] == "Wachtwoord" && $_GET['Wachtwoord1'] == "Herhaal wachtwoord"){
-     print("Je moet beide wachtwoord velden invoeren");
- }
-     if ( ($_GET['Wachtwoord1']) == $_GET['Wachtwoord2']){
-         print("mooi man");
+
+
+ /* hieronder is een check op de wachtwoorden -- Vincent */
+ if($_GET['Voornaam'] == "" && $_GET['Achternaam'] == ""){
+     print("Je moet de naam velden nog invullen <br>");
+
      }
-     else{
-         print("De wachtwoorden komen niet overheen");
+ /* SRC= https://www.w3schools.com/php/php_form_url_email.asp */
+     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_GET['Mail'])) {
+         print("Mailadress klopt niet <br>");
      }
- 
+
+     if ($_GET['Wachtwoord2'] == "" && $_GET['Wachtwoord1'] == "") {
+         print("Je moet beide wachtwoord velden invoeren <br>");
+     } else {
+         if (($_GET['Wachtwoord1']) == $_GET['Wachtwoord2']) {
+             print("mooi man");
+         } else {
+             print("De wachtwoorden komen niet overheen");
+         }
+     }
+
  ?>
 </div>
 </body>
