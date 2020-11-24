5c74b68251c4933351afa019fe7ad52955429e82
Vincent <70948074+MysteryFade@users.noreply.github.com>
2020-11-24 12:00:47 +0100

    begin login script

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <html>
 <?php
 include ('header.php');
+/*We hebbenn toesteming gekregen om oude wachtwoorden  over te slaan*/
 ?>
 <head>
 <style>
@@ -12,10 +13,38 @@ login {
 </head>
 <body>
 <div align="center" class="login">
-<form action="/logintest.php">
-<label for="fname" align="left">Gebruikersnaam:</label><br>
+<form method="get" action="login.php">
+<label for="fname" align="left">Emailadress:</label><br>
   <input type="text" id="ID" name="ID"><br>
-</form>
+<label for="fname" align="left">Wachtwoord:</label><br>
+  <input type="text" id="ID" name="Wachtwoord"><br>  
+ <input type="submit">
+</form> 
+<?php
+
+
+
+
+$WachtwoordInput = $_GET['Wachtwoord'];
+$UsernameInput = $_GET['ID'];
+print ($UsernameInput . $WachtwoordInput . "<br>");
+
+if(isset($_GET['ID'])){
+	global $Connection;
+	$query = mysqli_query($connection, "SELECT HashedPassword, LogonName FROM people WHERE '" . $UsernameInput . "' LIMIT 1");
+	/* */
+	 
+	
+	$rows = mysqli_num_rows($query);
+	 if($rows == 1){
+	 print("Hij werkt dus gewoon");
+	 }
+	 else
+	 {
+	 print("<h1>NEEN</h1>");
+	 }
+}
+?>
 </div>
 </body>
 </html>
