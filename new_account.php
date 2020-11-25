0c214f69873279c32140790058786f6c98469ca5
Vincent <70948074+MysteryFade@users.noreply.github.com>
2020-11-25 11:28:37 +0100

    Begin gemaakt SQL injectie informatie

--- new_account.php
+++ new_account.php
@@ -20,6 +20,7 @@ FORM {
 </style>
 </head>
 <body>
+<h1>Er moeten nog checks komen dus niet zomaar gebruiken</h1>
 <p></p>
 <div align="center" class="FORM">
 <form align="left" method="get" action="new_account.php">
@@ -48,10 +49,23 @@ FORM {
     }
     ?>
 
+    <label for="fname" align="left">Telefoonnummer:</label><br>
+    <input type="text" id="Telefoon" name="Telefoon" value="<?php print($_GET['Telefoon']);?>""><br>
+
+
+    <?php
+    if($_GET['Telefoon'] == "") {
+        print('<p class="error">U moet een telefoonnummer invullen</p>');
+    }
+    else{
+        print("<br>");
+    }
+    ?>
+
 <label for="fname" align="left">Emailadress:</label><br>
 
 
- <input type="text" id="Mail" name="Mail" value="<?php print($_GET['Mail']);?>"><br>
+ <input type="email" id="Mail" name="Mail" value="<?php print($_GET['Mail']);?>"><br>
     <?php
     /* Er moet nog een check komen voor email formaat*/
     $sqlmailcheck = mysqli_query($Connection, "SELECT EmailAddress FROM people WHERE EmailAddress = '" . $_GET['Mail'] . "' LIMIT 1");
@@ -119,8 +133,12 @@ FORM {
  $Achternaam = $_GET['Achternaam'];
 $Fullname =  ($Voornaam . " " . $Achternaam);
 $SearchName = ($Voornaam . " " . $Fullname);
- $Validfrom = (date("Y/m/d"));
- 
+ $Validfrom = (date("Y-m-d"));
+ print($Validfrom);
+ $Telefoonnummer = $_GET['Telefoon'];
+$Wachtwoord = $_GET['Wachtwoord1'];
+$Mail = $_GET['Mail'];
+
 
 
  /* INFORMATIE TESTEN
@@ -131,6 +149,12 @@ $SearchName = ($Voornaam . " " . $Fullname);
  print("Wachtwoord: " . $_GET['Wachtwoord1'] . "<br>");
  Print($Validfrom);
 */
+/*ER MOETEN NOG CHECKS KOMEN DUS NIET GEBRUIKEN */
+ $sqladdaccount = ("INSERT INTO people(PersonID, FullName, PreferredName, SearchName, IsPermittedToLogon, LogonName, HashedPassword, EmailAddress, ValidFrom, ValidTo)
+VALUES (" . $NewID . ", '" . $Fullname . "', '" . $Voornaam . "','" . $SearchName. "', 1, '" . $Mail ."', '" . $Wachtwoord . "', '" . $Mail . "', '" . $Validfrom . " 00:00:00', '9999-12-31 23:59:59')
+
+");
+ mysqli_query($Connection, $sqladdaccount);
  ?>
 
 </div>
