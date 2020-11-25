35fc4584eb710cfa38d4ad191c3dd81ce4bcfa3e
Vincent <70948074+MysteryFade@users.noreply.github.com>
2020-11-25 10:15:04 +0100

    Meerdere checks toegevoegd

--- new_account.php
+++ new_account.php
@@ -1,8 +1,16 @@
 <html>
 <?php
 include ('header.php');
+include_once "connect.php";
+global $Connection;
 /*We hebbenn toesteming gekregen om oude wachtwoorden  over te slaan*/
 ?>
+<style>
+    p.error {
+        color: red;
+        font-size: 12px;
+    }
+</style>
 <head>
 <style>
 FORM {
@@ -16,17 +24,56 @@ FORM {
 <div align="center" class="FORM">
 <form align="left" method="get" action="new_account.php">
 <label for="fname" align="left">Voornaam:</label><br>
-  <input type="text" id="Voornaam" name="Voornaam"><br><br>
+  <input type="text" id="Voornaam" name="Voornaam" value="<?php print($_GET['Voornaam']);?>"><br>
+
+    <?php
+    if($_GET['Voornaam'] == "") {
+        print('<p class="error">U moet een voornaam invullen</p>');
+    }
+    else{
+        print("<br>");
+    }
+    ?>
+
 <label for="fname" align="left">Achternaam:</label><br>
-  <input type="text" id="Achternaam" name="Achternaam"><br><br>
+  <input type="text" id="Achternaam" name="Achternaam" value="<?php print($_GET['Achternaam']);?>""><br>
+
+
+    <?php
+    if($_GET['Achternaam'] == "") {
+        print('<p class="error">U moet een achternaam invullen</p>');
+    }
+    else{
+        print("<br>");
+    }
+    ?>
+
 <label for="fname" align="left">Emailadress:</label><br>
- <input type="text" id="Mail" name="Mail"><br><br>
+
+
+ <input type="text" id="Mail" name="Mail" value="<?php print($_GET['Mail']);?>"><br>
+    <?php
+    /* Er moet nog een check komen voor email formaat*/
+    $sqlmailcheck = mysqli_query($Connection, "SELECT EmailAddress FROM people WHERE EmailAddress = '" . $_GET['Mail'] . "' LIMIT 1");
+    if($_GET['Mail'] != "") {
+        if (mysqli_num_rows($sqlmailcheck) == 0) {
+            $Uniekemail = TRUE;
+            print("<br>");
+
+        } else {
+            print("<p class='error'>Dit emailadress komt al voor bij ons</p>");
+            $Uniekemail = FALSE;
+        }
+    }
+    else{
+        print('<p class="error">U moet een emailadress invullen</p>');
+    }
+    ?>
  
- 
- 
+
 <label for="fname" align="left">Wachtwoord</label><br>
  <input type="text" id="Wachtwoord1" name="Wachtwoord1"><br><br>
- 
+
  <label for="fname" align="left">Herhaling wachtwoord</label><br>
  <input type="text" id="Wachtwoord2" name="Wachtwoord2"><br><br>
     <input type="submit">
@@ -36,14 +83,13 @@ FORM {
 
 
  /* hieronder is een check op de wachtwoorden -- Vincent */
+
  if($_GET['Voornaam'] == "" && $_GET['Achternaam'] == ""){
      print("Je moet de naam velden nog invullen <br>");
 
      }
  /* SRC= https://www.w3schools.com/php/php_form_url_email.asp */
-     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_GET['Mail'])) {
-         print("Mailadress klopt niet <br>");
-     }
+
 
      if ($_GET['Wachtwoord2'] == "" && $_GET['Wachtwoord1'] == "") {
          print("Je moet beide wachtwoord velden invoeren <br>");
