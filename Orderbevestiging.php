9371cbaf7d7efb480c3faf8402e430ab097ac130
Vincent <70948074+MysteryFade@users.noreply.github.com>
2020-12-09 20:52:26 +0100

    Update Orderbevestiging.php

--- Orderbevestiging.php
+++ Orderbevestiging.php
@@ -9,25 +9,34 @@ global $Connection;
   `PersonID` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idconsumer_orders`));
  */
-$sqlID = mysqli_query($Connection, "SELECT MAX(C_OrderID) AS ID FROM consumer_orders LIMIT 1");
-while($row = mysqli_fetch_assoc($sqlID)) {
-    $NewID = ($row['ID'] + 1);
-}
+if(!isset($_SESSION['Done'])) {
+    $sqlID = mysqli_query($Connection, "SELECT MAX(C_OrderID) AS ID FROM consumer_orders LIMIT 1");
+    while ($row = mysqli_fetch_assoc($sqlID)) {
+        $_SESSION['OrderID'] = ($row['ID'] + 1);
+    }
+
 #print $NewID;
-$user = 4000;
-if(isset($_SESSION['UserID'])) {
-    $user = $_SESSION['UserID'];
-}
 
-$Ordered = $_SESSION['mand'];
-$serieel = serialize($Ordered);
-$_SESSION['mand'] = [];
+    $user = 4000;
+    if (isset($_SESSION['UserID'])) {
+        $user = $_SESSION['UserID'];
+    }
+
+    $Ordered = $_SESSION['mand'];
+    $serieel = serialize($Ordered);
+    $_SESSION['mand'] = [];
 
 
-$sqlordertodatabase = ("INSERT INTO consumer_orders
-VALUES (" . $NewID . ", '" . $serieel . "', " . $user . ")");
+    $sqlordertodatabase = ("INSERT INTO consumer_orders VALUES (" . $_SESSION['OrderID'] . ", '" . $serieel . "', " . $user . ")");
     mysqli_query($Connection, $sqlordertodatabase);
+    $_SESSION['Done'] = TRUE;
+}
+$sqlItems = mysqli_query($Connection, "SELECT products FROM consumer_orders WHERE C_OrderID = '" . $_SESSION['OrderID'] . "' LIMIT 1");
 
+while($row = mysqli_fetch_assoc($sqlItems)) {
+    $BoughtProducts = unserialize($row['products']);
+}
+print_r($BoughtProducts);
 ?>
 <style>
     h1{
@@ -41,11 +50,19 @@ VALUES (" . $NewID . ", '" . $serieel . "', " . $user . ")");
     <div align="center">
 
 <h1>Betaling is gelukt!<br> Bedankt voor uw aankoop!</h1>
-<div style="background-color: white; color: black;width: 70%"
-
+<div style="background-color: white; color: black;width: 70%">
+    <h1 align="left" style="padding-left: 2%;">OrderInformatie</h1>
+    <h2 align="left" style="padding-left: 2%;">Gegevens</h2>
+    <p align="left" style="padding-left: 2%;">
+    Voornaam:    XXXX<br>
+    Achternaam:  XXXX<br>
+    Adress:      XXXX<br>
+    Postcode:    XXXX
+    </p>
     </div>
 </body>
 </html>
 <?php
+
 include __DIR__ . "/footer.php";
 ?>
