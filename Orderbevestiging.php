9dab56e3a74d7662f524c2eaad2944230fb4b8d7
Vincent <70948074+MysteryFade@users.noreply.github.com>
2020-12-09 21:09:27 +0100

    Update Orderbevestiging.php

--- Orderbevestiging.php
+++ Orderbevestiging.php
@@ -1,11 +1,12 @@
 <?php
 include __DIR__ . "/header.php";
 include_once "connect.php";
+include __DIR__ . "/functions.php";
 global $Connection;
 /*
    CREATE TABLE `nerdygadgets`.`consumer_orders` (
   `C_OrderID` INT NOT NULL,
-  `order` LONGTEXT NOT NULL,
+  `products` LONGTEXT NOT NULL,
   `PersonID` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idconsumer_orders`));
  */
@@ -37,6 +38,7 @@ while($row = mysqli_fetch_assoc($sqlItems)) {
     $BoughtProducts = unserialize($row['products']);
 }
 print_r($BoughtProducts);
+$totaalprijs = 0;
 ?>
 <style>
     h1{
@@ -50,7 +52,8 @@ print_r($BoughtProducts);
     <div align="center">
 
 <h1>Betaling is gelukt!<br> Bedankt voor uw aankoop!</h1>
-<div style="background-color: white; color: black;width: 70%">
+
+    <div style="border-bottom: black;">
     <h1 align="left" style="padding-left: 2%;">OrderInformatie</h1>
     <h2 align="left" style="padding-left: 2%;">Gegevens</h2>
     <p align="left" style="padding-left: 2%;">
@@ -59,6 +62,59 @@ print_r($BoughtProducts);
     Adress:      XXXX<br>
     Postcode:    XXXX
     </p>
+    <h2 align="left" style="padding-left: 2%;">Producten</h2>
+        <?php
+        foreach($BoughtProducts as $productid => $aantal){
+            $infoproduct = getProductInfo($productid);
+            $totaalprijsproduct = 0;
+
+            $Query = "
+                    SELECT ImagePath
+                    FROM stockitemimages 
+                    WHERE StockItemID = ?";
+
+            $Statement = mysqli_prepare($Connection, $Query);
+
+            mysqli_stmt_bind_param($Statement, "i", $productid);
+            mysqli_stmt_execute($Statement);
+            $R = mysqli_stmt_get_result($Statement);
+            $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
+
+            if ($R) {
+                $img = "Public/StockItemIMG/" . $R[0]['ImagePath'];
+                #print($img);
+            } else {
+
+                $img = "Public/StockGroupIMG/" . $infoproduct['BackupImagePath'];
+                #print ($img);
+            }
+
+            $totaalprijs = $totaalprijs + ($infoproduct["SellPrice"]* $aantal);
+            $totaalprijsproduct = ($infoproduct["SellPrice"]* $aantal);
+
+            print("
+                    
+                        <img src='https://media.tarkett-image.com/large/TH_25121916_25131916_25126916_25136916_001.jpg' width='100%' height='1px'>
+                            <img align ='left' class='media-object' src= '$img' style='width: 10%;'>
+                            
+                                <h3 align='right' class='media-heading'>" . $infoproduct["StockItemName"] . "</h3>
+                                
+                                <h4 align='right'>Prijs $aantal * " . $infoproduct["SellPrice"] . " = (" . $totaalprijsproduct . ")</strong></span>
+                            
+                        
+                    ");
+            #voorwaarde voor de quantiteit knop
+            if(isset($_POST["quantity"])){
+                if($_POST["quantity"] > 0 && $_POST["quantity"] <= $infoproduct["QuantityOnHand"]){
+                    $aantal = $_POST["quantity"];
+                    $productid = $_POST["productid"];
+                    $_SESSION["mand"][$productid] = $aantal;
+                }
+            }
+
+        }
+
+        ?>
     </div>
 </body>
 </html>
