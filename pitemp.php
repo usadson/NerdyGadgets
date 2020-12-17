b93c54ef3104339d96533fed99bda78df40c8028
Vincent <70948074+MysteryFade@users.noreply.github.com>
2020-12-17 14:38:56 +0100

    Pi item

---
+++ pitemp.php
@@ -0,0 +1,19 @@
+<?php
+include_once "connect.php";
+
+
+$SQLID = mysqli_query($Connection, "
+                    SELECT MAX(ColdRoomTemperatureID) AS ID
+                    FROM coldroomtemperatures");
+while ($row = mysqli_fetch_assoc($SQLID)) {
+    $ID = ($row['ID'] + 1);
+}
+print $ID;
+
+$temperatuur = $_GET['temp'];
+$deviceID = $_GET['ID'];
+$Date = date("Y-m-d H:i:s");
+$DateValidTo = "9999-12-31 23:59:59";
+print $Date;
+mysqli_query($Connection, "INSERT INTO coldroomtemperatures (ColdRoomTemperatureID, ColdRoomSensorNumber, RecordedWhen, Temperature, ValidFrom, ValidTo) VALUES (" .  $ID . ", " . $deviceID . ",  '" . $Date . "', " . $temperatuur . ", '" . $Date . "', '" . $DateValidTo . "') ");
+?>
