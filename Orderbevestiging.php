cf14211b070aa5a135f603851b3b01258939c02f
MisterMarv02 <72137118+MisterMarv02@users.noreply.github.com>
2021-01-26 13:08:53 +0100

    functie
    functie images

--- Orderbevestiging.php
+++ Orderbevestiging.php
@@ -122,23 +122,8 @@ $totaalprijs = 0;
             $infoproduct = getProductInfo($productid);
             $totaalprijsproduct = 0;
 
-            $Query = "
-                    SELECT ImagePath
-                    FROM stockitemimages 
-                    WHERE StockItemID = ?";
+            Image () 
 
-            $Statement = mysqli_prepare($Connection, $Query);
-
-            mysqli_stmt_bind_param($Statement, "i", $productid);
-            mysqli_stmt_execute($Statement);
-            $R = mysqli_stmt_get_result($Statement);
-            $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
-
-            if ($R) {
-                $img = "Public/StockItemIMG/" . $R[0]['ImagePath'];
-            } else {
-                $img = "Public/StockGroupIMG/" . $infoproduct['BackupImagePath'];
-            }
 
             $totaalprijs = $totaalprijs + ($infoproduct["SellPrice"] * $aantal);
             $totaalprijsproduct = ($infoproduct["SellPrice"] * $aantal);
