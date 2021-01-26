e2b85328505db9f7d5206405f5598092be57de71
Vincent <70948074+MysteryFade@users.noreply.github.com>
2021-01-26 13:41:39 +0100

    Revert "functie"
    This reverts commit e1d71f60a4c27695da9e5a7c30d2c80811aade61.

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@ function getProductInfo($id){
 }
 
 
-Function Image ($productid, $infoproduct, $Connection){
+Function Image ($productid, $infoproduct){
             $Query = "
                     SELECT ImagePath
                     FROM stockitemimages 
